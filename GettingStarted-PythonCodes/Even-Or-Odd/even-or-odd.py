7661a2bc9bb0d5c4992ea942ce2592715d48d833
Suhas Raut <suhasraut24@gmail.com>
2023-01-27 18:35:03 +0530

    Added Program file

---
+++ GettingStarted-PythonCodes/Even-Or-Odd/even-or-odd.py
@@ -0,0 +1,11 @@
+# Taking Input from User
+
+print("Enter The Number To Check Is It Even or Odd")
+number = int(input("Enter The Number To Check :"))
+
+# Check Whether The Number Is Even Or Not
+
+if (number % 2 == 0):
+    print("The Given Number is Even.")
+else:
+    print("The Given Number Is Odd.")
